d76d092598d1db744501f17ed90ad93d6f658fd7
GOLCLAN <ash@relaxedwebdesign.co.uk>
2015-05-14 13:16:28 +0100

    Revert "First file upload"
    This reverts commit 542d42aab186501854a728f1cbbca6cb6095ceb7.

--- GVL_Core/Libraries/Functions/Framework/Functions/functionList.hpp
+++
@@ -1,8 +0,0 @@
-
-	class Framework_Core {
-		file = "GVL_Core\Libraries\Functions\Framework\Functions";
-		class HeadlessClient{};
-		class PlayerSync{};
-		class RadioSettings{};
-		class RemoteExecuterHC{};
-	};
--- GVL_Core/Modules/MHQ/functionList.hpp
+++
@@ -1,5 +0,0 @@
-
-	class MHQ {
-		file = "GVL_Core\Modules\MHQ";
-		class MHQ_Init{};
-	};
