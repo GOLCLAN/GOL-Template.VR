0b6b815463d83a4565082919e3b201494633a397
GOLCLAN <ash@relaxedwebdesign.co.uk>
2016-03-28 13:43:21 +0100

    Update functionList.hpp

--- Core/Functions/Creation/functionList.hpp
+++ Core/Functions/Creation/functionList.hpp
@@ -1,8 +1,10 @@
 
 	class Creation {
 		file = "Core\Functions\Creation";
+		class CopyAIEdenZeus{};
 		class CopyLocation{};
 		class CopyObjects{};
 		class CreateAIStatic{};
+		class CreateAIStaticAutoGroup{};
 		class CreateObjects{};
 	};
